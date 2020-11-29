1cdc8e3a539fafc4e949d17cbad95ab1fed8fdf6
shen lu <shenlu@VPN-158-212.vpn.usf.edu>
2020-11-28 21:38:12 -0500

    taxi

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect
 
+
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,7 +17,7 @@ def about():
 
 @app.route('/taxi')
 def taxi():
-  return render_template('taxi.html')
+  return render_template('taxi.html', root=ROOT)
 
 
 if __name__ == '__main__':
