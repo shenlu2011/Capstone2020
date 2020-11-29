9e9f02f290f1631ed7551ad21591e3a153392e70
shen lu <shenlu@VPN-158-212.vpn.usf.edu>
2020-11-28 21:56:58 -0500

    taxi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect
 
 
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path="", static_folder="static")
 
 @app.route('/')
 def index():
