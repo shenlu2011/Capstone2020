aef939cddd89e779f38fac993b3f212263d3b543
shen lu <shenlu@VPN-158-212.vpn.usf.edu>
2020-11-29 15:48:49 -0500

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 
 
-app = Flask(__name__, static_url_path="/", static_folder="static")
+app = Flask(__name__, static_url_path="/", static_folder="Static")
 
 @app.route('/')
 def index():
